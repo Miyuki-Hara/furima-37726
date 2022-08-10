173c29f4e2911fc6bf2fac966e9b091855cb2e39
yuki-hatsuharu <yuki_iyashikeyogoto@icloud.com>
2022-08-10 15:03:13 +0900

    コメントアウト修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,12 +2,12 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
 
   belongs_to :user
-  has_one :purchase
+ # has_one :purchase
 
   belongs_to :category
   belongs_to :status
   belongs_to :delivery_charge
-  # belongs_to :prefecture
+  belongs_to :prefecture
   belongs_to :delivery_day
 
   has_one_attached :image
