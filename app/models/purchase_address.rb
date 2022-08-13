3e85d25893d543cd64cdbaa8c2b771ac38b41c43
yuki-hatsuharu <yuki_iyashikeyogoto@icloud.com>
2022-08-13 13:53:34 +0900

    テストコード実装
    * article.rbは不要のため削除

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -8,7 +8,7 @@ class PurchaseAddress
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :city
     validates :house_num
-    validates :phone_num, format: { with: /\A[0-9]{11}\z/, message: "is invalid." }
+    validates :phone_num, format: { with: /\A[0-9]{11}\z/, message: "is invalid" }
     validates :user_id
     validates :item_id
     validates :token
